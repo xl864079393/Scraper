04bd16546b1aa5a23902ff5e755643a51b612f9b
XiangLi <lx864079393@gmail.com>
2024-12-07 17:23:02 -0800

    fix bugs

--- Util/Container.py
+++ Util/Container.py
@@ -1,6 +1,5 @@
 from Util.DeltaEncoder import DeltaEncoder
 from collections import defaultdict
-from Util.rle_encode import rle_encode, rle_decode
 from bitarray import bitarray
 
 
@@ -44,9 +43,9 @@ class Container:
         self.insert(term, posting)
         #node.index, node.doc_encoder = self._delta_encode_postings(node.index)
 
-    def rle_encode_all_bitmap(self):
-        for key in self.bitmap.keys():
-            self.bitmap[key] = rle_encode(self.bitmap[key])
+    # def rle_encode_all_bitmap(self):
+    #     for key in self.bitmap.keys():
+    #         self.bitmap[key] = rle_encode(self.bitmap[key])
 
 
     def get_all_terms(self):
